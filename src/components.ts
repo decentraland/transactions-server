f622625c0b43ae120b3661f3b4dbd84c06c1dd71
Lautaro Petaccio <lausuper@gmail.com>
2023-01-13 17:26:22 -0300

    fix: Re-trigger deployment

--- src/components.ts
+++ src/components.ts
@@ -75,6 +75,7 @@ export async function initComponents(): Promise<AppComponents> {
     fetcher,
     collectionsSubgraph,
   })
+
   const transaction = createTransactionComponent({
     config,
     fetcher,
