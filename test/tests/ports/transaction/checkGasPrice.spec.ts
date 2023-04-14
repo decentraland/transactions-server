dd43bd8a169902d96c1a5bd029d15fc1e3bc6ca9
Gabriel Díaz <gabrieldiaz31@gmail.com>
2023-04-14 10:29:24 -0300

    feat: Verify if a contract method is allowed to skip max gas price allowed (#107)
    * feat: Add method to verify if a method txn is whitelisted from the max gas cap
    
    * feat: Add tests
    
    * style: Move lib imports to upper lines
    
    * feat: Refactor
    
    * fix: Catch when a not allowed method is sent to return false
    
    * feat: Update tests
    
    * fix: Does not log contract methods not allowed to skip max gas price check
    
    * fix: ESLint empty error

--- test/tests/ports/transaction/checkGasPrice.spec.ts
+++ test/tests/ports/transaction/checkGasPrice.spec.ts
@@ -1,13 +1,23 @@
 import { Response } from 'node-fetch'
 import { checkGasPrice } from '../../../../src/ports/transaction/validation/checkGasPrice'
 import { test } from '../../../components'
+import { TransactionData } from '../../../../src/ports/transaction/types'
+import TransactionDataMock from '../../../mocks/transactionData'
 
 test('checkGasPrice component', function ({ components }) {
+  let transactionData: TransactionData
+
   describe('when checking the gas price for a txn', () => {
     describe('and the FF MAX_GAS_PRICE_ALLOWED_IN_WEI is turned on', () => {
+      let contractsMock: jest.SpyInstance
+
       beforeEach(() => {
-        const { features } = components
+        const { contracts, features } = components
+        transactionData = { ...TransactionDataMock.setManager }
         jest.spyOn(features, 'getIsFeatureEnabled').mockResolvedValueOnce(true)
+        contractsMock = jest
+          .spyOn(contracts, 'isCollectionAddress')
+          .mockResolvedValueOnce(false)
       })
 
       describe('and the max gas price allowed is defined', () => {
@@ -38,7 +48,9 @@ test('checkGasPrice component', function ({ components }) {
           })
 
           it('should not throw an error', async () => {
-            await expect(checkGasPrice(components)).resolves.not.toThrow()
+            await expect(
+              checkGasPrice(components, transactionData)
+            ).resolves.not.toThrow()
           })
         })
 
@@ -57,7 +69,9 @@ test('checkGasPrice component', function ({ components }) {
           })
 
           it('should throw an error', async () => {
-            await expect(checkGasPrice(components)).rejects.toThrow()
+            await expect(
+              checkGasPrice(components, transactionData)
+            ).rejects.toThrow()
           })
         })
 
@@ -70,14 +84,49 @@ test('checkGasPrice component', function ({ components }) {
           })
 
           it('should throw an error', async () => {
-            await expect(checkGasPrice(components)).rejects.toThrow()
+            await expect(
+              checkGasPrice(components, transactionData)
+            ).rejects.toThrow()
           })
         })
       })
 
       describe('and the max gas price allowed is not defined', () => {
         it('should throw an error', async () => {
-          await expect(checkGasPrice(components)).rejects.toThrow()
+          await expect(
+            checkGasPrice(components, transactionData)
+          ).rejects.toThrow()
+        })
+      })
+
+      describe('and the txn contract method is allowed to skip max gas price', () => {
+        describe('and the txn is an approve of the collection manager to spend mana on behalf of the user', () => {
+          it('should not throw an error', async () => {
+            await expect(
+              checkGasPrice(components, TransactionDataMock.approveMana)
+            ).resolves.not.toThrow()
+          })
+        })
+
+        describe('and the txn is an allowance of the collection manager to create collections using the CollectionFactoryV3', () => {
+          it('should not throw an error', async () => {
+            await expect(
+              checkGasPrice(components, TransactionDataMock.createCollection)
+            ).resolves.not.toThrow()
+          })
+        })
+
+        describe('and the txn allow/disallow the collection store to mint items', () => {
+          beforeEach(() => {
+            contractsMock.mockReset()
+            contractsMock.mockResolvedValueOnce(true)
+          })
+
+          it('should not throw an error', async () => {
+            await expect(
+              checkGasPrice(components, TransactionDataMock.setMinters)
+            ).resolves.not.toThrow()
+          })
         })
       })
     })
@@ -89,7 +138,9 @@ test('checkGasPrice component', function ({ components }) {
       })
 
       it('should not throw an error', async () => {
-        await expect(checkGasPrice(components)).resolves.not.toThrow()
+        await expect(
+          checkGasPrice(components, transactionData)
+        ).resolves.not.toThrow()
       })
     })
   })
